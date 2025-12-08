39bda91f05e7c3fcedf24d6c6ccb26bcd6e488f7
Alberto Fructos Acedo <afructosa01@educarex.es>
2025-12-08 14:08:39 +0100

    Subclase "VtcEV" realizada

--- VtcEV.java
+++ VtcEV.java
@@ -1,3 +1,5 @@
+import java.util.List;
+import java.util.Iterator;
 
 /**
  * Write a description of class VtcEV here.
@@ -12,5 +14,79 @@ public class VtcEV extends ElectricVehicle{
             
          super(company, location, targetLocation, name, plate, batteryCapacity);                 
          setType(VehicleTier.VTC);                 
+    }
+  
+    @Override
+    public void calculateRoute() {
+        EVCompany company = getCompany();
+        Location location = getLocation();
+        Location targetLocation = getTargetLocation();
+        int batteryLevel = getBatteryLevel();
+        int batteryCapacity = getBatteryCapacity();
+
+        int distanciaDestino = location.distance(targetLocation);
+        int energiaNecesaria = distanciaDestino * EVDemo.COSTEKM;
+
+        ChargingStation mejor = null;
+        ChargingStation fallback = null;
+        double mejorTarifa = 0.0; 
+        boolean primera = true;
+
+        if (batteryLevel < energiaNecesaria) {
+            List<ChargingStation> stations = company.getCityStations();
+            Iterator<ChargingStation> it = stations.iterator();
+
+            while (it.hasNext()) {
+                ChargingStation st = it.next();
+                Location locSt = st.getLocation();
+                int d1 = location.distance(locSt);
+                int energiaHastaEstacion = d1 * EVDemo.COSTEKM;
+
+                boolean llegoAEstacion = batteryLevel >= energiaHastaEstacion;
+                boolean esFallback = (d1 == 0 && batteryLevel == batteryCapacity);
+
+                if (llegoAEstacion) {
+                    boolean tieneValido = false;
+                    double mejorTarifaEnEstacion = 0.0;
+
+                    Iterator<Charger> itCh = st.getChargers().iterator();
+                    while (itCh.hasNext()) {
+                        Charger ch = itCh.next();
+                        if (ch instanceof StandardCharger || ch instanceof SolarCharger) {
+                            double tarifa = ch.getChargingFee();
+
+                            if (!tieneValido || tarifa < mejorTarifaEnEstacion) {
+                                mejorTarifaEnEstacion = tarifa;
+                                tieneValido = true;
+                            }
+                        }
+                    }
+
+                    if (tieneValido) {
+                        if (esFallback) {
+                            if (fallback == null) {
+                                fallback = st;
+                            }
+                        } else {
+                            if (primera || mejorTarifaEnEstacion < mejorTarifa) {
+                                mejor = st;
+                                mejorTarifa = mejorTarifaEnEstacion;
+                                primera = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (mejor == null && fallback != null) {
+            mejor = fallback;
+        }
+
+        if (mejor != null) {
+            setRechargingLocation(mejor.getLocation());
+        } else {
+            setRechargingLocation(null);
+        }
     }
 }
